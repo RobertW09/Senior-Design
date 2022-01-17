872a5b3d0753745370f56f61b8bb9e317700592c
Robert <rswallaceva@verizon.net>
2022-01-16 20:29:12 -0500

    Android Intro Tutorial
    Followed intro tutorial on developer.android.com. Adds a text and button to change activities. Builds fundamental android studio knowledge.

--- AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/MainActivity.java
+++ AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.heartbyteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.heartbyteapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the "Send" Button */
+    public void sendMessage(View view) {
+        // Do something in response to the button press.
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
