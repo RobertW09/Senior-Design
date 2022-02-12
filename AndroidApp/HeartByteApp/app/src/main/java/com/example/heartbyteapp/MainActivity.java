b01fc57ae9da0c5c088c005063d710e739212e02
RobertW09 <rswallaceva@verizon.net>
2022-02-11 23:10:11 -0500

    WiP UI Skeleton
    Work in progress skeleton for UI - continuing to make Spinner and include and label additional buttons.

--- AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/MainActivity.java
+++ AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public static final String EXTRA_MESSAGE = "com.example.heartbyteapp.MESSAGE";
 
@@ -15,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup Spinner
+        Spinner mode_spin = findViewById(R.id.mode_spinner);
+        mode_spin.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.modes_array));
+        mode_spin.setAdapter(adapter);
     }
 
-    /** Called when the user taps the "Send" Button */
+    /*
+     * Called when the user taps the "Send" Button
+     */
     public void sendMessage(View view) {
         // Do something in response to the button press.
         Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -25,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
