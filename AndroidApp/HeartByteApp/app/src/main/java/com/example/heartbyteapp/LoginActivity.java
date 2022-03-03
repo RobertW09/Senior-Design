1b6e57485b4ef88fb3c536fa9508d4caa8bd4af5
George Gomez <georgeg5326@gmail.com>
2022-03-03 14:05:57 -0500

    update 2

--- AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/LoginActivity.java
+++ AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/LoginActivity.java
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
     }
 
     void login(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+
     }
 }
