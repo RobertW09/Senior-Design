599ae24cb6d7b39e6f6f3e25a571d0201a54be7d
George Gomez <georgeg5326@gmail.com>
2022-04-07 13:53:50 -0400

    settings page frame work done

--- AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/SettingsActivity.java
+++ AndroidApp/HeartByteApp/app/src/main/java/com/example/heartbyteapp/SettingsActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-
+    // bottom  nav buttons
     private Button SettingsButton;
     private Button HomeButton;
     private Button DashboardButton;
+
+    //settings buttons
+    private Button InfoSettings;
+    private Button FamilySettings;
+    private Button LogoutSettings;
+
 
 
     @Override
@@ -29,6 +36,16 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         HomeButton.setOnClickListener(this);
         DashboardButton = (Button) findViewById(R.id.dashboard_button);
         DashboardButton.setOnClickListener(this);
+
+        // settings buttons
+        InfoSettings = (Button) findViewById(R.id.settings_user_info_button);
+        InfoSettings.setOnClickListener(this);
+        FamilySettings = (Button) findViewById(R.id.settings_family_button);
+        FamilySettings.setOnClickListener(this);
+        LogoutSettings = (Button) findViewById(R.id.settings_logout_button);
+        LogoutSettings.setOnClickListener(this);
+
+
 
         // Setup Toolbar
         Toolbar toolbar = (Toolbar)findViewById(R.id.main_toolbar);
